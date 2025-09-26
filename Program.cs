75ea3235c98920789197ac9e441d7cd76e6cf0b1
Your Name <jolle@MacBook-Pro-de-Jonathan.local>
2025-09-26 17:47:35 +0200

    commit program.cs commit Readme.md

--- Program.cs
+++ Program.cs
@@ -2,6 +2,61 @@
 
 List<IUser> users = new List<IUser>();
 
-users.Add(new NormalUser("JolleJ@gmail.com", "password123"));
+users.Add(new User("JolleJ@gmail.com", "password123"));
 
-IUser? active_user = null;
+IUser? active_user = null; // en tom plats för att hålla reda på den som är inloggad
+
+bool running = true;
+
+while (running)
+{
+    Console.Clear();
+
+    if (active_user == null)
+    {
+        Console.Clear();
+        Console.Write("Username:");
+        string username = Console.ReadLine();
+
+        Console.Clear();
+        Console.Write("Password");
+        string password = Console.ReadLine();
+        Console.Clear();
+
+
+        foreach (IUser user in users)
+        {
+            if (user.TryLogin(username, password))
+            {
+                active_user = user;
+                break;
+            }
+        }
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to the inventory:");
+
+        switch (active_user.GetRole())
+        {
+            case Role.User:
+                Console.WriteLine("Welcome User");
+                break;
+        }
+
+        if (active_user.IsRole(Role.User))
+        {
+            Console.WriteLine("Upload information about your item");
+            Console.WriteLine("What type is you item?");
+            string Type = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("What Rarity is your item?");
+            string Rarity = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("How much damage does it got?");
+            int Damage = int.Parse(Console.ReadLine());
+        }
+    }
+
+}
